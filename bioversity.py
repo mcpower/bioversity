2142bc7bbaf28ee1d43104bad5d5a3d44c03d04e
Emery Mitchell <emery@aegis.moe>
2014-10-16 00:24:40 -0400

    initial commit

---
+++ bioversity.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+#coding: utf-8
+import random
+import re
+from twython import Twython
+
+consumer_key = ""
+consumer_key_secret = ""
+access_token = ""
+access_token_secret = ""
+
+twitter = Twython(consumer_key, consumer_key_secret, access_token, access_token_secret)
+
+ADJECTIVE = ["Trans", "Shy", "Asexual", "Neuroatypical", "Plural", "Cute", "19-year-old", "Cuddly", "Music-loving", "Funny", "Warm-hearted", "Multiple", "Silly", "Goofy", "Awkward", "Autistic", "Radical"]
+DESCRIPTOR = ["girl", "student", "catgirl", "wannabe", "woman", "SJW", "activist", "punk", "magical girl", "punk rock princess", "witch", "protagonist", "clutz"]
+ACTION = ["studying software engineering", "doing the best I can", "trying to change the world", "fighting for my friends", "befriending nice people", "lurking on a forum somewhere", "relearning empathy and kindness", "eating Chinese food", "probably baking desserts", "singing too loudly in the shower", "stumbling over code", "stubbing her toe", "watching anime", "eating Italian food", "eating sweets", "curled up, asleep"]
+RELATIONSHIP = [ \
+    "wife to @enbykid", \
+    "headmate to @kinuko_chan & @SomethingCole", \
+    "DPS to @tteugeo", \
+    "adventuring partner to @m1sp", \
+    "programming fangirl to @FioraAeterna", \
+    "sister to @MinaZenunim", \
+    "mom to everybody", \
+    "kouhai to @Dirk_Gently", \
+    "senpai to @wincom_plus", \
+    "support to @hoodiejoy", \
+    "healer to @SeventhRondo", \
+    "brand witch to @SaulKewl", \
+    "future roommate to @plus_chan", \
+    "bestie to @RijoPorter", \
+    "terror to #GamerGate", \
+    "antagonist to bigots", \
+    "mistress of memes", \
+    "scourge to America", \
+    "winner of Mario Kart", \
+    "mayor of Animal Crossing" \
+]
+
+def generate(max_length):
+	terminator = "Your friend"
+	length_tolerance = 10
+
+        random.shuffle(ADJECTIVE)
+        random.shuffle(DESCRIPTOR)
+        random.shuffle(ACTION)
+        random.shuffle(RELATIONSHIP)
+
+        next_relationship = RELATIONSHIP.pop()
+        next_relationship = re.sub('([a-zA-Z])', lambda x: x.groups()[0].upper(), next_relationship, 1)
+
+        bio = "{0} {1} {2}. {3}".format(ADJECTIVE.pop(), DESCRIPTOR.pop(), ACTION.pop(), next_relationship)
+
+	while len(bio) < (max_length - length_tolerance):
+                if len(RELATIONSHIP) == 0:
+                        return attempt
+                next_relationship = RELATIONSHIP.pop()
+		attempt = "{0}, {1}. {2}.".format(bio, next_relationship, terminator)
+		if len(attempt) > max_length:
+			continue
+		elif len(attempt) > max_length - length_tolerance:
+			return attempt
+		bio += ", {0}".format(next_relationship)
+
+desc = generate(140)
+twitter.update_profile(description=desc)
